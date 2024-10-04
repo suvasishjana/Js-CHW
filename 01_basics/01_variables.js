5cc83fecea1d8611d8345ea2beb4cf0fba4f2c7f
suvasishjana <93206320+suvasishjana@users.noreply.github.com>
2024-10-04 16:31:29 +0000

    variables commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId="21632";
+let accountEmail="sh@ghsh.com";
+var accountPassword="12345";
+accountCity="Kolkata";
+let accountState;
+
+// console.log(x);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
