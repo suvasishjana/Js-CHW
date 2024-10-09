555a72af241a67b46152037208dda16efa956245
suvasishjana <93206320+suvasishjana@users.noreply.github.com>
2024-10-09 04:48:07 +0000

    strings commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Assassin-Creed-origins')
+
+// console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "  suvasish jana   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','&&&'));
+
+
+const laugh="yohoo! "
+
+console.log(`Brook laughs like ${laugh.repeat(4)}`);
+console.log(gameName.split('-'));
+
+ const x="ajkvcnaahoa" 
+ console.log(x.slice(10,4));
+ 
+
